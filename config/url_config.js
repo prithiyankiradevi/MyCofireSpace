29782264b4b64066d1518e6c47394a30d23f0b6c
prithiyankiradevi <prithiyankiradevi.s@firebeetechnoservices.com>
2022-03-25 13:21:52 +0530

    second commit

--- config/url_config.js
+++ config/url_config.js
@@ -1,3 +1,5 @@
 module.exports={
-    url:'mongodb://localhost:27017/myCoFireSpace'
+    // url:'mongodb://localhost:27017/myCoFireSpace'
+    url:"mongodb+srv://coinsqueens:coinsqueens001234@cluster0.zycyh.mongodb.net/myCoFireSpace?retryWrites=true&w=majority"
+   
 }
