667bdc59dbfb11696cf0286fa91fea4db1f8f097
prithiyankiradevi <prithiyankiradevi.s@firebeetechnoservices.com>
2022-04-12 16:44:17 +0530

    12/04/2022 first

--- controller/instereste_controller.js
+++ controller/instereste_controller.js
@@ -2,30 +2,33 @@ const userModel=require('../model/user_model')
 const spaceModel=require('../model/space_model')
 const interested=require('../model/interest_model')
 const { default: mongoose } = require('mongoose')
+const jwt=require('jsonwebtoken')
 const razorPay=require('razorpay')
 
 const createInterestedPersons=async(req,res)=>{
     try{
-        const user=await interested.interested.aggregate([{$unwind:{path:"$spaceDetails"}},{$unwind:{path:"$customerDetails"}},{$match:{$and:[{"spaceDetails._id":new mongoose.Types.ObjectId(req.params.spaceId)},{"customerDetails._id":new mongoose.Types.ObjectId(req.params.userId)}]}}])
-        if(user.length==0){
-            if(req.params.interest=='true'){
-                req.body.interest=true
-                const user=await userModel.aggregate([{$match:{"_id":new mongoose.Types.ObjectId(req.params.userId)}},{$match:{"deleteFlag":false}}])
-                req.body.customerDetails=user
-                const space=await spaceModel.space.aggregate([{$match:{"_id":new mongoose.Types.ObjectId(req.params.spaceId)}},{$match:{"deleteFlag":false}}])       
-                req.body.spaceDetails=space
-                interested.interested.create(req.body,(err,data)=>{
-                if(err){throw err}
-                else{
-                    res.status(200).send({success:'true',message:'created successfully',data})
+        if(req.headers.authorization){
+            const token=jwt.decode(req.headers.authorization)
+            const user=await interested.interested.aggregate([{$unwind:{path:"$spaceDetails"}},{$unwind:{path:"$customerDetails"}},{$match:{$and:[{"spaceDetails._id":new mongoose.Types.ObjectId(req.params.spaceId)},{"customerDetails._id":new mongoose.Types.ObjectId(token.id)}]}}])
+            if(user.length==0){
+                if(req.body.interest=='true'){
+                    const user=await userModel.aggregate([{$match:{"_id":new mongoose.Types.ObjectId(token.id)}},{$match:{"deleteFlag":false}}])
+                    req.body.customerDetails=user
+                    const space=await spaceModel.space.aggregate([{$match:{"_id":new mongoose.Types.ObjectId(req.params.spaceId)}},{$match:{"deleteFlag":false}}])       
+                    req.body.spaceDetails=space
+                    interested.interested.create(req.body,(err,data)=>{
+                    if(err){throw err}
+                    else{
+                        res.status(200).send({success:'true',message:'created successfully',data})
+                    }
+                })
                 }
-            })
+            }else{
+                const data=await interested.interested.findByIdAndUpdate({_id:user[0]._id},req.body,{new:true})
+                res.status(200).send({success:'true',message:'unliked successfully',data})
             }
         }else{
-            req.body.interest=req.params.interest
-            req.body.deleteFlag=true
-            const data=await interested.interested.findByIdAndUpdate({_id:user[0]._id},req.body,{new:true})
-            res.status(200).send({success:'true',message:'remove successfully',data})
+            res.status(200).send({success:'false',message:'unauthorized',data})
         }
     }catch(e){
         console.log(e.message)
@@ -49,8 +52,9 @@ const getInterestedUserBasedOnSpace=async(req,res)=>{
 
 const buyersFavoriteList=async(req,res)=>{
     try{
-        if(req.params.userId){
-            const data=await interested.interested.aggregate([{$unwind:{path:"$customerDetails"}},{$match:{$and:[{"customerDetails._id":new mongoose.Types.ObjectId(req.params.userId)},{"deleteFlag":false}]}}])
+        if(req.headers.authorization){
+            const token=jwt.decode(req.headers.authorization)
+            const data=await interested.interested.aggregate([{$unwind:{path:"$customerDetails"}},{$match:{$and:[{"customerDetails._id":new mongoose.Types.ObjectId(token.id)},{"deleteFlag":false}]}}])
             res.status(200).send({success:'true',message:'fetch data successfully',data})
         }else{
             res.status(401).send({success:'false',message:'params is required',data:[]})
--- route/interested_route.js
+++ route/interested_route.js
@@ -1,9 +1,9 @@
 const router=require('express').Router()
 const interest=require('../controller/instereste_controller')
 
-router.get('/create/:userId/:spaceId/:interest',interest.createInterestedPersons)
+router.post('/create/:spaceId',interest.createInterestedPersons)
 router.get('/getInterestedUserBasedSpace/:spaceId',interest.getInterestedUserBasedOnSpace)
-router.get('/getSingleInterestedUser/:userId',interest.buyersFavoriteList)
+router.get('/getSingleInterestedUser',interest.buyersFavoriteList)
 router.get('/getSingleUserForSpaceOwner/:spaceId/:userId',interest.getSingleInterestedUserForSpaceOwner)
 
 
